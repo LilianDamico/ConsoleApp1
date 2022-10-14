9e40f328441209c0fb41b3dd5a7837e033182241
wfons <wfons@LAPTOP-TPCF6RBK>
2022-10-14 10:46:38 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿int f, n, c;
+n = 0;
+f = 1;
+Console.WriteLine("Informe o valor a ser fatorado: ");
+n = int.Parse(Console.ReadLine());
+if (n<0)
+{
+    Console.WriteLine("Numero inválido. Apenas numeros maiores ou iguais a zero.");
+}
+else if((n==0)|| (n==1))
+{
+    Console.WriteLine("Fatorial de {0} é 1", n);
+}
+else
+{
+    for(c = n; c >= 1; c-- )
+    {
+        f*= c;
+    }
+    Console.WriteLine("Fatorial de {0} é {1} ", n, f);
+}
+Console.ReadKey();
